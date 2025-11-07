5b832c334437a6890b5adadb3c08f090782cc2f5
KumarNaikMude7 <101415336+KumarNaikMude7@users.noreply.github.com>
2025-11-07 13:36:39 +0530

    Update amazonlinux-jenkins.sh

--- amazonlinux-jenkins.sh
+++ amazonlinux-jenkins.sh
@@ -5,3 +5,12 @@ sudo yum install java-21-amazon-corretto -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+
+#Trouble shooting steps to make executors run on Amazon Linux
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
